3e4fe337f0e05356cbb7f73538c4457fafcfab83
PurviDarekar <61351292+PurviDarekar@users.noreply.github.com>
2020-12-21 14:10:59 +0530

    Update importTestData.js

--- lab01/solution/importTestData.js
+++ lab01/solution/importTestData.js
@@ -2,7 +2,16 @@ const {readFile}  = require('fs').promises;
 const {promisify} = require('util');
 const parse       = promisify(require('csv-parse'));
 const {Firestore} = require('@google-cloud/firestore');
+const {Logging} = require('@google-cloud/logging'); 
+const logName = 'pet-theory-logs-importTestData';
 
+// Creates a Logging client
+const logging = new Logging();
+const log = logging.log(logName);
+
+const resource = {
+  type: 'global',
+};
 if (process.argv.length < 3) {
   console.error('Please include a path to a csv file');
   process.exit(1);
@@ -49,3 +58,6 @@ async function importCsv(csvFileName) {
 }
 
 importCsv(process.argv[2]).catch(e => console.error(e));
+success_message = `Success: importTestData - Wrote ${records.length} records`
+const entry = log.entry({resource: resource}, {message: `${success_message}`});
+log.write([entry]);
